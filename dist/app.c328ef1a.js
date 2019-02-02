94b7a2b02f0723556f8f485dab4151703d1e8818
Vartik12 <vartik17802371@gmail.com>
2019-02-02 13:32:10 +0530

    pushing parcel

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -24595,7 +24595,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59401" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "62940" + '/');
 
   ws.onmessage = function (event) {
     var data = JSON.parse(event.data);
